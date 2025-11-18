a0b64639b26d7b1507f4066654ef4a1e5f84e70e
Hussein <husseinbassam148@gmail.com>
2025-11-18 07:32:32 +0300

    feat(ui): created a slide switch control for theme control

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,22 +2,43 @@
 import { useTheme } from "../context/ThemeContext";
 
 /**
- * @description A button component used to switch between light and dark themes.
- * It uses an icon to indicate the current state (Sun for Light, Moon for Dark).
+ * @description A slide switch component used to switch between light and dark themes.
  */
 const ThemeToggle = () => {
   const { isDark, toggleTheme } = useTheme();
-
-  return (
+   return (
+    <div className="mt-[6px] mr-2">
     <button
       onClick={toggleTheme}
+      className="
+        relative h-10 w-24 rounded-full
+        bg-blue-100 dark:bg-gray-800
+        transition-colors duration-500 ease-out
+        overflow-hidden
+      "
     >
-      {isDark ? (
-        <h1 className="text-white">why so espresso depresso homie? :)</h1>
-      ) : (
-        <h1 className="text-white">h1 :)</h1>
-      )}
+      {/* Background circle */}
+      <div
+        className="
+          absolute left-4 top-1/2 -translate-y-1/2
+          h-6 w-6 rounded-full bg-yellow-300
+          dark:bg-gray-500
+          transition-all duration-500
+        "
+      />
+
+      {/* Sliding disc */}
+      <div
+        className={`
+          absolute top-1/2 -translate-y-1/2
+          h-6 w-6 rounded-full
+          bg-gray-900 dark:bg-black
+          transition-all duration-700 ease-out
+          ${isDark === true ? "left-4" : "left-16"}
+        `}
+      />
     </button>
+    </div>
   );
 };
 
